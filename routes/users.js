c105d65c12309068ac1634a5277782c7c22b42c3
Mahesh5d5 <89468423+Mahesh5d5@users.noreply.github.com>
2021-10-27 22:34:01 -0500

    changes made to the users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+values = 0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  values =values+1;
+  res.send('User accesses are: '+values);
 });
 
 module.exports = router;
